150b6c309741c4448e084be287cf655a7f811813
Franck Barry <jaimsmuzik@gmail.com>
2026-06-19 14:49:22 +0200

    feat: initCv

---
+++ api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+   import json
+
+   app = FastAPI()
+
+   @app.get("/")
+   def read_root():
+       return {"status": "online", "message": "Welcome to my Cloud-Native CV API"}
+
+   @app.get("/api/cv")
+   def get_cv():
+       with open("cv_data.json", "r") as file:
+           data = json.load(file)
+       return data
