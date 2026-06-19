e6c041ff3d4a61d3b6fef50428154c1c035ba119
Franck Barry <jaimsmuzik@gmail.com>
2026-06-19 16:12:21 +0200

    feat: modif-api

--- api/main.py
+++ api/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+
+# Configuration CORS pour autoriser le futur frontend à interroger l'API
 @app.get("/")
 def read_root():
     return {"status": "online", "message": "Welcome to my Cloud-Native CV API"}
