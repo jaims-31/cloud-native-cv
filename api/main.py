9d363b95e891b9a1b5f64e795a86c710e38c2b63
Franck Barry <jaimsmuzik@gmail.com>
2026-06-19 15:41:08 +0200

    feat: jsons

--- api/main.py
+++ api/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
-   import json
+import json
 
-   app = FastAPI()
+app = FastAPI()
 
-   @app.get("/")
-   def read_root():
-       return {"status": "online", "message": "Welcome to my Cloud-Native CV API"}
 
-   @app.get("/api/cv")
-   def get_cv():
-       with open("cv_data.json", "r") as file:
-           data = json.load(file)
-       return data
+@app.get("/")
+def read_root():
+    return {"status": "online", "message": "Welcome to my Cloud-Native CV API"}
+
+
+@app.get("/api/cv")
+def get_cv():
+    with open("cv_data.json", "r") as file:
+        data = json.load(file)
+    return data
